4b21007d3de0e33beb9d5dcda0dd90f32063f42c
Svitlana Karahayevae <svitlana.karahayeva@gmail.com>
2021-10-26 22:37:38 -0400

    Adding Treatment model, views: index and detail

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.fields import IntegerField
+
 
 # Create your models here.
+
+
+CATEGORIES = (
+    (1, 'Facial and Skin Treatments'),
+    (2, 'Massage'),
+    (3, 'Lazer Treatments'),
+    (4, 'Hand and Foot Treatments'),
+    (5, 'Hair Services')
+)
+
+class Treatment(models.Model):
+    name = models.CharField(max_length=100)
+    category = IntegerField(
+        choices=CATEGORIES,
+        default=[1][0]
+    )
+    image = models.CharField(max_length=300)
+    description = models.TextField(max_length=800)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Treatment
+
 
 # Create your views here.
 
@@ -7,3 +11,13 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+  #Treatments views
+def treatments_index(request):
+  treatments = Treatment.objects.all()
+  return render(request, 'treatments/index.html', { 'treatments': treatments })
+
+def treatments_detail(request, treatment_id):
+  treatment = Treatment.objects.get(id=treatment_id)
+  return render(request, 'treatments/detail.html', { 'treatment': treatment })
+
