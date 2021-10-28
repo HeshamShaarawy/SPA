86c4c869d44b7bdbaf4887727e83cecbdba30586
Hesham <hesham2go@outlook.com>
2021-10-27 23:49:51 -0400

    fixed index-bug

--- main_app/models.py
+++ main_app/models.py
@@ -67,6 +67,10 @@ class Treatment(models.Model):
     image = models.CharField(max_length=300)
     description = models.TextField(max_length=800)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    def __str__(self):
+        return f"{self.name} - {self.get_category_display()} - ${self.price}"
+    def get_absolute_url(self):
+        return reverse('treatments_detail', kwargs={'treatment_id': self.id})
 
 
 class Client(models.Model):
@@ -78,11 +82,7 @@ class Client(models.Model):
         default=[0][0]
     )
 
-    def __str__(self):
-        return f"{self.name} - {self.get_category_display()} - ${self.price}"
 
-    def get_absolute_url(self):
-        return reverse('treatments_detail', kwargs={'treatment_id': self.id})
 
 
 class Booking(models.Model):
